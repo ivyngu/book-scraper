87a299b14636ac2475fd54e9d1c9943d04173d8f
ivy <ivyng@seas.upenn.edu>
2023-08-16 22:55:32 -0400

    rewrite with playwright

--- bs.js
+++ bs.js
@@ -1,68 +1,63 @@
-import { Client } from "@notionhq/client";
-import fetch from 'node-fetch';
-import { load } from 'cheerio';
-import prompt_ from 'prompt-sync';
-import * as dotenv from 'dotenv';
+import playwright from 'playwright'; // scrapping book info
+import prompt from 'prompt'; // prompt user for goodreads website
+import { Client } from "@notionhq/client"; // for adding page to notion db
+import dotenv from 'dotenv'; // needed for reading env file
 
-// get api key
-dotenv.config();
-const NOTION_API_KEY = process.env.API_KEY;
-const notion = new Client({ auth: NOTION_API_KEY });
-
-// get goodreads info for scraping
-const prompt = prompt_();
-const prompt_url = prompt("Goodreads URL: ");
-const url = prompt_url;
-
-const database_id = "778516b4cdd340d3ad745b5b13c75f82";
-//const database_id = db.slice(22, 54);
-
-// scrape goodreads for book info
-const response = await fetch(url);
-const body = await response.text();
-let $ = load(body);
-
-// loading in jsonLD info
-const jsonRaw = $("script[type='application/ld+json']")[0].children[0].data; 
-const result = JSON.parse(jsonRaw);
-const authors = result.author[0];
-
-let title = result.name;
-let author = authors.name;
-let cover = result.image;
-
-// create a page in notion database with scraped book info as properties
 (async () => {
-  const response = await notion.pages.create({
-    parent: {
-      database_id: database_id,
-    },
-  	cover: {
-  		type: "external",
-    	external: {
-    		url: cover
-    	}
-  	},
-    properties: {
-      Name: {
-        title: [
-          {
-            text: {
-              content: title
+    
+    prompt.start();
+    dotenv.config();
+    
+    const p = await prompt.get(['link']);
+    const url = p.link;
+    
+    const browser = await playwright.chromium.launch({headless: true}); 
+    const context = await browser.newContext(); 
+    const page = await context.newPage(); 
+    
+    await page.goto(url);
+    await page.waitForTimeout(5000); // wait for 5 seconds to load up
+    
+    let author = await page.locator('div.ContributorLinksList > span > a > span.ContributorLink__name').textContent();
+    let title = await page.locator('div.BookPageTitleSection__title > h1').textContent();
+    const cover = await page.$eval(".ResponsiveImage", img => img.src);
+    
+    await browser.close();
+    
+    // get notion info
+    const notion = new Client({   auth: process.env.NOTION_TOKEN, });
+    const database_id = process.env.NOTION_DATABASE;
+    const response = await notion.pages.create({
+        parent: {
+            database_id: database_id,
+        },
+        cover: {
+            type: "external",
+            external: {
+                url: cover
+            }
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: title
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-      "Author": {
-        rich_text: [
-          {
-            text: {
-              content: author
+            "Author": {
+                rich_text: [
+                    {
+                        text: {
+                            content: author
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    },
-  });
-  console.log(response);
-  })();
+        },
+    });
+    console.log(response);
+})();
+
